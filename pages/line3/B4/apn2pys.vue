f77dd83d6b0be73400985dc983ee5f1523690945
Sgss <103869422+Sgss123@users.noreply.github.com>
2024-04-21 20:40:17 +0800

    更新

--- pages/line3/B4/apn2pys.vue
+++ pages/line3/B4/apn2pys.vue
@@ -42,104 +42,104 @@
 				stationRes: [{
 					title: '机场北（2号航站楼）',
 					subtitle: 'Airport N.(Terminal 2)',
-					src: 'https://download.zhengxian.top/l5whpxg/Jiaokou.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2apn/AirportN.mp3'
 				}, {
 					title: '机场南（1号航站楼）',
 					subtitle: 'Airport S.(Terminal 1)',
-					src: 'https://download.zhengxian.top/l5whpxg/Tanwei.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/AirportS.mp3'
 				}, {
 					title: '高增',
 					subtitle: 'Gaozeng',
-					src: 'https://download.zhengxian.top/l5whpxg/Zhongshanba.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Gaozeng.mp3'
 				}, {
 					title: '人和',
 					subtitle: 'Renhe',
-					src: 'https://download.zhengxian.top/l5whpxg/Xichang.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Renhe.mp3'
 				}, {
 					title: '龙归',
 					subtitle: 'Longgui',
-					src: 'https://download.zhengxian.top/l5whpxg/Xicun.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Longgui.mp3'
 				}, {
 					title: '嘉禾望岗',
 					subtitle: 'Jiahewanggang',
-					src: 'https://download.zhengxian.top/l5whpxg/GuangzhouRailwayStation.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Jiahewanggang.mp3'
 				}, {
 					title: '白云大道北',
 					subtitle: 'Baiyundadaobei',
-					src: 'https://download.zhengxian.top/l5whpxg/Xiaobei.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Baiyundadaobei.mp3'
 				}, {
 					title: '永泰',
 					subtitle: 'Yongtai',
-					src: 'https://download.zhengxian.top/l5whpxg/Taojin.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Yongtai.mp3'
 				}, {
 					title: '同和',
 					subtitle: 'Tonghe',
-					src: 'https://download.zhengxian.top/l5whpxg/Ouzhuang.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Tonghe.mp3'
 				}, {
 					title: '京溪南方医院',
 					subtitle: 'Jingxi Nanfang Hospital',
-					src: 'https://download.zhengxian.top/l5whpxg/Zoo.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/JingxiNanfangHospital.mp3'
 				}, {
 					title: '梅花园',
 					subtitle: 'Meihuayuan',
-					src: 'https://download.zhengxian.top/l5whpxg/Yangji.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Meihuayuan.mp3'
 				}, {
 					title: '燕塘',
 					subtitle: 'Yantang',
-					src: 'https://download.zhengxian.top/l5whpxg/Wuyangcun.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Yantang.mp3'
 				}, {
 					title: '广州东站',
 					subtitle: 'Guangzhou East Railway Station',
-					src: 'https://download.zhengxian.top/l5whpxg/ZhujiangNewTown.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/GuangzhouEastRailwayStation.mp3'
 				}, {
 					title: '林和西',
 					subtitle: 'Linhexi',
-					src: 'https://download.zhengxian.top/l5whpxg/Liede.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Linhexi.mp3'
 				}, {
 					title: '体育西路',
 					subtitle: 'Tiyu Xilu',
-					src: 'https://download.zhengxian.top/l5whpxg/Tancun.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/TiyuXilu.mp3'
 				}, {
 					title: '珠江新城',
 					subtitle: 'Zhujiang New Town',
-					src: 'https://download.zhengxian.top/l5whpxg/Yuancun.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/ZhujiangNewTown.mp3'
 				}, {
 					title: '广州塔',
 					subtitle: 'Canton Tower',
-					src: 'https://download.zhengxian.top/l5whpxg/KeyunLu.mp3',
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/CantonTower.mp3',
 
 				}, {
 					title: '客村',
 					subtitle: 'Kecun',
-					src: 'https://download.zhengxian.top/l5whpxg/Chebeinan.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Kecun.mp3'
 				}, {
 					title: '大塘',
 					subtitle: 'Datang',
-					src: 'https://download.zhengxian.top/l5whpxg/Dongpu.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Datang.mp3'
 				}, {
 					title: '沥滘',
 					subtitle: 'Lijiao',
-					src: 'https://download.zhengxian.top/l5whpxg/Sanxi.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Lijiao.mp3'
 				}, {
 					title: '厦滘',
 					subtitle: 'Xiajiao',
-					src: 'https://download.zhengxian.top/l5whpxg/Yuzhu.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Xiajiao.mp3'
 				}, {
 					title: '大石',
 					subtitle: 'Dashi',
-					src: 'https://download.zhengxian.top/l5whpxg/Dashadi.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Dashi.mp3'
 				}, {
 					title: '汉溪长隆',
 					subtitle: 'Hanxi Changlong',
-					src: 'https://download.zhengxian.top/l5whpxg/Dashadong.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/HanxiChanglong.mp3'
 				}, {
 					title: '石桥',
 					subtitle: 'Shiqiao',
-					src: 'https://download.zhengxian.top/l5whpxg/Wenchong.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/Shiqiao.mp3'
 				}, {
 					title: '番禺广场',
 					subtitle: 'Panyu Square',
-					src: 'https://download.zhengxian.top/l5whpxg/Shuangsha.mp3'
+					src: 'https://vip.123pan.cn/1812581465/broadcast/3/B4/2pys/PanyuSquare.mp3'
 				}]
 			}
 		},
