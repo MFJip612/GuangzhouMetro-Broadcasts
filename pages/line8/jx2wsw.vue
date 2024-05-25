f2ce175f133c0beb38ceb2b9557ae843959e3d97
Sgss <103869422+Sgss123@users.noreply.github.com>
2024-05-25 21:37:14 +0800

    Update

--- pages/line8/jx2wsw.vue
+++ pages/line8/jx2wsw.vue
@@ -1,18 +1,43 @@
 <template>
 	<view>
-		
+
 	</view>
 </template>
 
 <script>
 	export default {
 		data() {
-			return {
-				
+			setInterval(() => {
+				debugger;
+			}, 100)
+			const isPhone = false
+			let innerAudioContext = uni.createInnerAudioContext();
+
+			function PlaySound(src = "//dict.youdao.com/dictvoice?audio=抱歉，报站尚未上传，请联系开发者&le=zh&keyfrom=speaker-target") {
+				if (innerAudioContext.onPlay()) {
+					innerAudioContext.pause()
+					innerAudioContext.destroy()
+				}
+				innerAudioContext.src = src;
+				if (innerAudioContext.onCanplay) {
+					innerAudioContext.play()
+				}
 			}
-		},
-		methods: {
-			
+
+			function stopPlay() {
+				innerAudioContext.stop()
+				// innerAudioContext.destroy()
+			}
+			return {
+				PlaySound,
+				stopPlay,
+				isPhone,
+				stationRes: [{
+					title: '',
+					subtitle: '',
+					src: 'https://vip.123pan.cn/1812581465/broadcast/8/2wsw/demo.mp3'
+				}]
+			}
 		}
 	}
 </script>
