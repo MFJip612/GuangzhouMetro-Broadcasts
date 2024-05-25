ffa99fed486c1f6ee4159225f2acb9b0a69ca3ac
Sgss <103869422+Sgss123@users.noreply.github.com>
2024-05-25 21:37:49 +0800

    Update jx2wsw.vue

--- pages/line8/jx2wsw.vue
+++ pages/line8/jx2wsw.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-
+		<uni-group v-for="item in stationRes" :title="item.title+'    '+item.subtitle" top="20" mode="card">
+			<view>{{item.title}} 站</view>
+			<view>{{item.subtitle}} Station</view>
+			<button @click="PlaySound(item.src)">报站</button>
+			<button class="stop-play" @click="stopPlay()">停止播放</button>
+		</uni-group>
 	</view>
 </template>
 
