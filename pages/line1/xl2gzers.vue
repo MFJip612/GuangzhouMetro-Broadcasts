3a554b71f8e84305a7468c85eec3447c18cde872
Sgss <103869422+Sgss123@users.noreply.github.com>
2024-06-21 23:35:16 +0800

    Update URL

--- pages/line1/xl2gzers.vue
+++ pages/line1/xl2gzers.vue
@@ -36,67 +36,67 @@
 				stationRes: [{
 					title: '西塱',
 					subtitle: 'Xilang',
-					src: 'https://download.zhengxian.top/l1bzgbc/Xilang.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Xilang.mp3'
 				}, {
 					title: '坑口',
 					subtitle: 'Kengkou',
-					src: 'https://download.zhengxian.top/l1bzgbc/Kengkou.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Kengkou.mp3'
 				}, {
 					title: '花地湾',
 					subtitle: 'Huadiwan',
-					src: 'https://download.zhengxian.top/l1bzgbc/Huadiwan.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Huadiwan.mp3'
 				}, {
 					title: '芳村',
 					subtitle: 'Fangcun',
-					src: 'https://download.zhengxian.top/l1bzgbc/Fangcun.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Fangcun.mp3'
 				}, {
 					title: '黄沙',
 					subtitle: 'Huangsha',
-					src: 'https://download.zhengxian.top/l1bzgbc/Huangsha.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Huangsha.mp3'
 				}, {
 					title: '长寿路',
 					subtitle: 'Changshoulu',
-					src: 'https://download.zhengxian.top/l1bzgbc/Changshoulu.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Changshoulu.mp3'
 				}, {
 					title: '陈家祠',
 					subtitle: 'Chen Clan Academy',
-					src: 'https://download.zhengxian.top/l1bzgbc/Chenjiaci.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Chenjiaci.mp3'
 				}, {
 					title: '西门口',
 					subtitle: 'Ximenkou',
-					src: 'https://download.zhengxian.top/l1bzgbc/Ximenkou.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Ximenkou.mp3'
 				}, {
 					title: '公园前',
 					subtitle: 'Gongyuanqian',
-					src: 'https://download.zhengxian.top/l1bzgbc/Gongyuanqian.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Gongyuanqian.mp3'
 				}, {
 					title: '农讲所',
 					subtitle: 'Peasant Movement Institute',
-					src: 'https://download.zhengxian.top/l1bzgbc/Nongjiangsuo.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Nongjiangsuo.mp3'
 				}, {
 					title: '烈士陵园',
 					subtitle: 'Martyrs\' Park',
-					src: 'https://download.zhengxian.top/l1bzgbc/Lieshilingyuan.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Lieshilingyuan.mp3'
 				}, {
 					title: '东山口',
 					subtitle: 'Dongshankou',
-					src: 'https://download.zhengxian.top/l1bzgbc/Dongshankou.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Dongshankou.mp3'
 				}, {
 					title: '杨箕',
 					subtitle: 'Yangji',
-					src: 'https://download.zhengxian.top/l1bzgbc/Yangji.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Yangji.mp3'
 				}, {
 					title: '体育西路',
 					subtitle: 'Tiyu Xilu',
-					src: 'https://download.zhengxian.top/l1bzgbc/Tiyuxilu.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Tiyuxilu.mp3'
 				}, {
 					title: '体育中心',
 					subtitle: 'Tianhe Sports Center',
-					src: 'https://download.zhengxian.top/l1bzgbc/Tiyuzhongxin.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Tiyuzhongxin.mp3'
 				}, {
 					title: '广州东站',
 					subtitle: 'Guangzhou East Railway Station',
-					src: 'https://download.zhengxian.top/l1bzgbc/Guangzhoudongzhan.mp3'
+					src: 'https://vip.123pan.cn/1829011389/azl/gb/l1gzd/Guangzhoudongzhan.mp3'
 				}]
 			}
 		},
