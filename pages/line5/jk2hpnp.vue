f225be8ad9bb76cd676c8c6ab319f425f8cb9b5f
Sgss <103869422+Sgss123@users.noreply.github.com>
2024-07-04 16:09:26 +0800

    Add New Page

--- pages/line5/jk2hpnp.vue
+++ pages/line5/jk2hpnp.vue
@@ -1,9 +1,9 @@
 <template>
 	<view>
 		<uni-group v-for="item in stationRes" :title="item.title+'    '+item.subtitle" top="20" mode="card">
-			<view>{{item.title}} 站</view>
+			<view class="text-info">{{item.title}} 站</view>
 			<view>{{item.subtitle}} Station</view>
-			<button @click="PlaySound(item.src)">报站</button>
+			<button class="btn btn-primany" @click="PlaySound(item.src)">报站</button>
 			<button class="stop-play" @click="stopPlay()">停止播放</button>
 		</uni-group>
 	</view>
