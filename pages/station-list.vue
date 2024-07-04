c50bcd9990566f00920bed2af65e0e47b18a65da
Sgss <103869422+Sgss123@users.noreply.github.com>
2024-07-04 16:18:10 +0800

    修改新模板

--- pages/station-list.vue
+++ pages/station-list.vue
@@ -1,6 +1,14 @@
 <template>
 	<view>
-		
+		<uni-group v-for="item in stationRes" :title="item.title+'    '+item.subtitle" top="20" mode="card">
+			<view class="text-info">{{item.title}} 站</view>
+			<view id="english">{{item.subtitle}} Station</view>
+			<view id="btn" class="">
+				<button class="btn btn-primany" @click="PlaySound(item.src)">报站</button>
+				<button class="stop-play" @click="stopPlay()">停止播放</button>
+			</view>
+
+		</uni-group>
 	</view>
 </template>
 
@@ -8,15 +16,18 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+#btn{
+	display: flex;
+	flex-direction: row;
+}
 </style>
