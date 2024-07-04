72be4ea194de8606fddc7e07365435c0cab19da4
Sgss <103869422+Sgss123@users.noreply.github.com>
2024-07-04 16:24:49 +0800

    Update station-list.vue

--- pages/station-list.vue
+++ pages/station-list.vue
@@ -1,8 +1,8 @@
 <template>
 	<view>
 		<uni-group v-for="item in stationRes" :title="item.title+'    '+item.subtitle" top="20" mode="card">
-			<view class="text-info">{{item.title}} 站</view>
-			<view id="english">{{item.subtitle}} Station</view>
+			<view id="chinese" class="fs-1">{{item.title}} 站</view>
+			<view id="english" class="fs-2">{{item.subtitle}} Station</view>
 			<view id="btn" class="">
 				<button class="btn btn-primany" @click="PlaySound(item.src)">报站</button>
 				<button class="stop-play" @click="stopPlay()">停止播放</button>
@@ -26,9 +26,9 @@
 </script>
 
 <style lang="scss" scoped>
-#btn{
-	display: flex;
-	flex-direction: row;
-	justify-content: space-around;
-}
+	#btn {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+	}
 </style>
