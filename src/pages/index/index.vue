da4efc9e085a0ee785d12f913560a2494deeebec
MFJip <103869422+Sgss123@users.noreply.github.com>
2024-09-30 23:26:39 +0800

    Update index.vue

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -154,9 +154,9 @@
 		</view>
 		<view class="text-area"
 			style="justify-content: center;display: flex;flex-direction: column;align-items: center;">
-			<text class="title">由 <uni-link href="https://blog.zhengxian.top/" text="380AM-0204"></uni-link>
+			<text class="title">由 <uni-link href="https://blog.zhengxian.top/" text="@380AM-0204">@380AM-0204</uni-link>
 				提供文件服务</text>
-			<text class="title">由 <uni-link href="https://space.bilibili.com/9033373/" text="红茶w"></uni-link>
+			<text class="title">由 <uni-link href="https://space.bilibili.com/9033373/" text="@红茶w">@红茶w</uni-link>
 				提供报站资源</text>
 		</view>
 	</view>
@@ -180,178 +180,177 @@
 
 <style>
 	.content {
-	    display: flex;
-	    flex-direction: column;
-	    align-items: center;
-	    justify-content: center;
-	}
-	
-	.title {
-	    font-size: 36rpx;
-	    color: #8f8f94;
-	}
-	
-	.choose {
-	    width: 750rpx;
-	    display: flex;
-	    flex-direction: row;
-	    align-items: center;
-	    flex-wrap: wrap;
-	    justify-content: space-around;
-	    margin: 50rpx;
-	}
-	
-	.line {
-	    margin: 10rpx;
-	    position: relative;
-	    width: 280rpx;
-	    border-radius: 20rpx;
-	    display: flex;
-	    text-align: center;
-	    justify-content: center;
-	    align-items: center;
-	    color: #FFF;
-	}
-	
-	.line .num {
-	    position: relative;
-	    text-align: center;
-	    font-size: 4rem;
-	    font-family: 'Arial';
-	}
-	
-	.line .col {
-	    position: relative;
-	    display: flex;
-	    flex-direction: column;
-	    text-align: center;
-	}
-	
-	.line .col .cn {
-	    font-size: 2rem;
-	    font-family: '黑体';
-	}
-	
-	.line .col .en {
-	    font-size: 1.2rem;
-	    font-family: 'Arial';
-	}
-	
-	.color-white {
-	    color: #FFF;
-	}
-	
-	.color-black {
-	    color: #000;
-	}
-	
-	.line-1 {
-	    background-color: #F3D03E;
-	}
-	
-	.line-2 {
-	    color: #FFF;
-	    background-color: #00629B;
-	}
-	
-	.line-3 {
-	    color: #FFF;
-	    background-color: #ECA154;
-	}
-	
-	.line-4 {
-	    color: #FFF;
-	    background-color: #00843D;
-	}
-	
-	.line-5 {
-	    color: #FFF;
-	    background-color: #C5003E;
-	}
-	
-	.line-6 {
-	    color: #FFF;
-	    background-color: #80225F;
-	}
-	
-	.line-7 {
-	    color: #FFF;
-	    background-color: #97D700;
-	}
-	
-	.line-8 {
-	    color: #FFF;
-	    background-color: #008C95;
-	}
-	
-	.line-9 {
-	    background-color: #71CC98;
-	}
-	
-	.line-13 {
-	    color: #FFF;
-	    background-color: #8E8C13;
-	}
-	
-	.line-14 {
-	    color: #FFF;
-	    background-color: #81312F;
-	}
-	
-	.line-18 {
-	    color: #FFF;
-	    background-color: #0047BA;
-	}
-	
-	.line-21 {
-	    color: #FFF;
-	    background-color: #211747;
-	}
-	
-	.line-22 {
-	    color: #FFF;
-	    background-color: #CD5228;
-	}
-	
-	.line-gf {
-	    height: 4.5834rem;
-	    color: #FFF;
-	    background-color: #C4D600;
-	}
-	
-	.line-gf .col .cn {
-	    font-size: 2.3rem;
-	}
-	
-	.line-gf .col .en {
-	    font-size: 1.25rem;
-	}
-	
-	.line-apm {
-	    height: 4.5834rem;
-	    color: #FFF;
-	    background-color: #00B5E2;
-	}
-	
-	.line-apm .num {
-	    font-size: 2.85rem;
-	}
-	
-	.intercity {
-	    height: auto;
-	    padding: 1% 1%;
-	    width: auto;
-	    color: #FFF;
-	    background-color: rgb(42, 90, 166);
-	    border-radius: 10rpx;
-	}
-	
-	.intercity .col .cn {
-	    font-size: 2.1rem;
-	}
-	
-	.intercity .col .en {
-	    font-size: 1.1rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
 	}
 
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+
+	.choose {
+		width: 750rpx;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		margin: 50rpx;
+	}
+
+	.line {
+		margin: 10rpx;
+		position: relative;
+		width: 280rpx;
+		border-radius: 20rpx;
+		display: flex;
+		text-align: center;
+		justify-content: center;
+		align-items: center;
+		color: #FFF;
+	}
+
+	.line .num {
+		position: relative;
+		text-align: center;
+		font-size: 4rem;
+		font-family: 'Arial';
+	}
+
+	.line .col {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+	}
+
+	.line .col .cn {
+		font-size: 2rem;
+		font-family: '黑体';
+	}
+
+	.line .col .en {
+		font-size: 1.2rem;
+		font-family: 'Arial';
+	}
+
+	.color-white {
+		color: #FFF;
+	}
+
+	.color-black {
+		color: #000;
+	}
+
+	.line-1 {
+		background-color: #F3D03E;
+	}
+
+	.line-2 {
+		color: #FFF;
+		background-color: #00629B;
+	}
+
+	.line-3 {
+		color: #FFF;
+		background-color: #ECA154;
+	}
+
+	.line-4 {
+		color: #FFF;
+		background-color: #00843D;
+	}
+
+	.line-5 {
+		color: #FFF;
+		background-color: #C5003E;
+	}
+
+	.line-6 {
+		color: #FFF;
+		background-color: #80225F;
+	}
+
+	.line-7 {
+		color: #FFF;
+		background-color: #97D700;
+	}
+
+	.line-8 {
+		color: #FFF;
+		background-color: #008C95;
+	}
+
+	.line-9 {
+		background-color: #71CC98;
+	}
+
+	.line-13 {
+		color: #FFF;
+		background-color: #8E8C13;
+	}
+
+	.line-14 {
+		color: #FFF;
+		background-color: #81312F;
+	}
+
+	.line-18 {
+		color: #FFF;
+		background-color: #0047BA;
+	}
+
+	.line-21 {
+		color: #FFF;
+		background-color: #211747;
+	}
+
+	.line-22 {
+		color: #FFF;
+		background-color: #CD5228;
+	}
+
+	.line-gf {
+		height: 4.5834rem;
+		color: #FFF;
+		background-color: #C4D600;
+	}
+
+	.line-gf .col .cn {
+		font-size: 2.3rem;
+	}
+
+	.line-gf .col .en {
+		font-size: 1.25rem;
+	}
+
+	.line-apm {
+		height: 4.5834rem;
+		color: #FFF;
+		background-color: #00B5E2;
+	}
+
+	.line-apm .num {
+		font-size: 2.85rem;
+	}
+
+	.intercity {
+		height: auto;
+		padding: .5%;
+		width: auto;
+		color: #FFF;
+		background-color: rgb(42, 90, 166);
+		border-radius: 10rpx;
+	}
+
+	.intercity .col .cn {
+		font-size: 2.1rem;
+	}
+
+	.intercity .col .en {
+		font-size: 1.1rem;
+	}
 </style>
