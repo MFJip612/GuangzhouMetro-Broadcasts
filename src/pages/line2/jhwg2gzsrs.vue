c90cd977d659af1815521d18dc58b8932375a76c
MFYip <qingqi.tan@qq.com>
2024-11-09 02:52:30 +0800

    添加2号线广州南站方向

---
+++ src/pages/line2/jhwg2gzsrs.vue
@@ -0,0 +1,103 @@
+<template>
+	<view>
+		<uni-group v-for="item in stationRes" :key="item.title" :title="item.title + '    ' + item.subtitle" top="20"
+			mode="card" class="station">
+			<view>{{ item.title }} 站</view>
+			<view>{{ item.subtitle }} Station</view>
+			<view class="btdGroup">
+				<button @click="PlaySound(item.src)">报站</button>
+				<button class="stop-play" @click="stopPlay()">停止播放</button>
+			</view>
+		</uni-group>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				isPhone: false,
+				innerAudioContext: null,
+				stationRes: [] // 初始化stationRes为一个空数组
+			};
+		},
+		onLoad() {
+			// 初始化 innerAudioContext
+			this.innerAudioContext = uni.createInnerAudioContext();
+
+			// 获取站点资源
+			this.getStationRes();
+		},
+		methods: {
+			async getStationRes() {
+				try {
+					const response = await fetch(
+						"https://broadcast-1304995454.cos.ap-guangzhou.myqcloud.com/resource.json");
+					if (!response.ok) {
+						throw new Error(`HTTP error! Status: ${response.status}`);
+					}
+					const data = await response.json();
+					const {
+						stationRes
+					} = data;
+					const filteredStations = stationRes.filter(station =>
+						station.destination.includes("GuangzhouSouthRailwayStation", "JiahewanggangStart"));
+					this.stationRes = filteredStations;
+				} catch (error) {
+					console.error('Failed to fetch or filter station resources:', error);
+				}
+			},
+			PlaySound(src) {
+				if (this.innerAudioContext.onPlay) {
+					this.innerAudioContext.pause();
+					this.innerAudioContext.destroy();
+				}
+				this.innerAudioContext.src = src;
+				if (this.innerAudioContext.onCanplay) {
+					this.innerAudioContext.play();
+				}
+			},
+			stopPlay() {
+				this.innerAudioContext.pause();
+			}
+		}
+	}
+</script>
+
+<style>
+	.stations {
+		color: #fff;
+		background-color: #00629b;
+	}
+
+	/* 一般认为宽度大于等于768px的设备为平板 */
+	@media screen and (min-width: 768px) {
+
+		/* 平板样式 */
+	}
+
+	/* 一般认为宽度小于768px的设备为手机 */
+	@media screen and (max-width: 767px) {
+		/* 手机样式 */
+	}
+
+	.btnGroup {
+		display: flex;
+		justify-content: center;
+	}
+
+	button {
+		width: 200rpx;
+		display: flex;
+		justify-content: center;
+	}
+
+
+	.btn-d {
+		display: flex;
+	}
+
+	.station {
+		width: auto;
+	}
+</style>
---
+++ src/pages/line2/line2.vue
@@ -0,0 +1,63 @@
+<template>
+	<!-- <navigator url="/pages/line2/gzsrs2jhwg" delta="1">
+		<view class="line">
+			<view class="cn">
+				<text>广州南站——嘉禾望岗</text>
+			</view>
+			<view class="en">
+				<text>From Guangzhou South Railway Station towards Jiahewanggang</text>
+			</view>
+		</view>
+	</navigator> -->
+	<navigator url="/pages/line2/jhwg2gzsrs" delta="1">
+		<view class="line">
+			<view class="cn">
+				<text>嘉禾望岗——广州南站</text>
+			</view>
+			<view class="en">
+				<text>From Jiahewanggang towards Guangzhou South Railway Station</text>
+			</view>
+		</view>
+	</navigator>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	navigator {
+		width: initial;
+	}
+
+	.line {
+		margin: 50rpx auto;
+		padding: 10rpx;
+		width: max-content;
+		color: #fff;
+		background-color: #00629B;
+		border-radius: 20rpx;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+
+		.cn {
+			font-family: '黑体';
+			font-size: 2rem;
+		}
+
+		.en {
+			font-family: 'Arial';
+			font-size: 1.2rem;
+		}
+	}
+</style>
