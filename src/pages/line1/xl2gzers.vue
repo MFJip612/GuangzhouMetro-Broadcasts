21453cf8bb9894787f07071f145134b7ecf9dc03
MFYip <qingqi.tan@qq.com>
2024-11-09 03:00:46 +0800

    Update xl2gzers.vue

--- src/pages/line1/xl2gzers.vue
+++ src/pages/line1/xl2gzers.vue
@@ -32,7 +32,7 @@
 			async getStationRes() {
 				try {
 					const response = await fetch(
-						"https://broadcast-1304995454.cos.ap-guangzhou.myqcloud.com/l1_resource.json");
+						"https://broadcast-1304995454.cos.ap-guangzhou.myqcloud.com/resource.json");
 					if (!response.ok) {
 						throw new Error(`HTTP error! Status: ${response.status}`);
 					}
