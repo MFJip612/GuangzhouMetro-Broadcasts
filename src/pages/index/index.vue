036d2e3f4add11ecf44258e923ae6fbd8738c365
MFYip <qingqi.tan@qq.com>
2024-11-09 17:21:33 +0800

    Update README.md

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -196,7 +196,7 @@
 	}
 
 	.choose {
-		width: 20rem;
+		width: 25rem;
 		display: flex;
 		flex-direction: row;
 		align-items: center;
---
+++ src/pages/line3/line3.vue
@@ -0,0 +1,55 @@
+<template>
+	<view>
+		<navigator url="/pages/line3/B4/index" delta="1">
+			<view class="line">
+				<view class="cn">
+					<text>B4列车报站</text>
+				</view>
+				<view class="en">
+					<text>B4</text>
+				</view>
+			</view>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	navigator {
+		width: initial;
+	}
+
+	.line {
+		margin: 50rpx auto;
+		padding: 10rpx;
+		width: max-content;
+		color: #fff;
+		background-color: #ECA154;
+		border-radius: 20rpx;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+
+		.cn {
+			font-family: '黑体';
+			font-size: 2rem;
+		}
+
+		.en {
+			font-family: 'Arial';
+			font-size: 1.2rem;
+		}
+	}
+</style>
