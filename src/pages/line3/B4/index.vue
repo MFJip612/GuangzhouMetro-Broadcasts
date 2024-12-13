186ebe15270f2366e073e9b9925cb7bae83438cc
MFJip <103869422+Sgss123@users.noreply.github.com>
2024-12-13 23:38:36 +0800

    添加页面

--- src/pages/line3/B4/index.vue
+++ src/pages/line3/B4/index.vue
@@ -1,19 +1,65 @@
 <template>
 	<view>
-		
+		<navigator url="/pages/line3/B4/apr2pys" delta="1">
+			<view class="line">
+				<view class="cn">
+					<text>机场北——番禺广场</text>
+				</view>
+				<view class="en">
+					<text>Airport N. towards Panyu Square</text>
+				</view>
+			</view>
+		</navigator>
+		<navigator url="/pages/line3/B4/apr2hb" delta="1">
+			<view class="line">
+				<view class="cn">
+					<text>机场北——海傍</text>
+				</view>
+				<view class="en">
+					<text>Airport N. towards Haibang</text>
+				</view>
+			</view>
+		</navigator>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
+export default {
+	data() {
+		return {
+
+		};
 	}
+}
 </script>
 
 <style lang="scss">
+navigator {
+	width: initial;
+}
 
+.line {
+	margin: 50rpx auto;
+	padding: 10rpx;
+	width: max-content;
+	color: #fff;
+	background-color: #ECA154;
+	border-radius: 20rpx;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+
+	.cn {
+		font-family: '黑体';
+		font-size: 2rem;
+	}
+
+	.en {
+		font-family: 'Arial';
+		font-size: 1.2rem;
+	}
+}
 </style>
