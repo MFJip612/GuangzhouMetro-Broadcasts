dfe4aec4ac74a48bbd30b7810499cd8ef20a2830
15625104818 <103869422+Sgss123@users.noreply.github.com>
2024-12-21 18:34:33 +0800

    添加css

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import './uni.promisify.adaptor'
+import '7.css/dist/7.css'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
