34f0e3b8231e9a8cae368c792990ffd5fea6e4e1
15625104818 <103869422+Sgss123@users.noreply.github.com>
2025-01-07 20:50:04 +0800

    Add a new page

--- src/pages/line2/line2.vue
+++ src/pages/line2/line2.vue
@@ -1,24 +1,26 @@
 <template>
-	<!-- <navigator url="/pages/line2/gzsrs2jhwg" delta="1">
-		<view class="line">
-			<view class="cn">
-				<text>广州南站——嘉禾望岗</text>
+	<view>
+		<navigator url="/pages/line2/gzsrs2jhwg" delta="1">
+			<view class="line">
+				<view class="cn">
+					<text>广州南站——嘉禾望岗</text>
+				</view>
+				<view class="en">
+					<text>Guangzhou South Railway Station towards Jiahewanggang</text>
+				</view>
 			</view>
-			<view class="en">
-				<text>From Guangzhou South Railway Station towards Jiahewanggang</text>
+		</navigator>
+		<navigator url="/pages/line2/jhwg2gzsrs" delta="1">
+			<view class="line">
+				<view class="cn">
+					<text>嘉禾望岗——广州南站</text>
+				</view>
+				<view class="en">
+					<text>Jiahewanggang towards Guangzhou South Railway Station</text>
+				</view>
 			</view>
-		</view>
-	</navigator> -->
-	<navigator url="/pages/line2/jhwg2gzsrs" delta="1">
-		<view class="line">
-			<view class="cn">
-				<text>嘉禾望岗——广州南站</text>
-			</view>
-			<view class="en">
-				<text>Jiahewanggang towards Guangzhou South Railway Station</text>
-			</view>
-		</view>
-	</navigator>
+		</navigator>
+	</view>
 </template>
 
 <script>
