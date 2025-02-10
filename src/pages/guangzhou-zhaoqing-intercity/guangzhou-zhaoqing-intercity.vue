68f9578dec4e86c9f490191bed0581e6c948adb8
15625104818 <103869422+Sgss123@users.noreply.github.com>
2025-02-10 11:14:43 +0800

    Add Intercity

---
+++ src/pages/guangzhou-zhaoqing-intercity/guangzhou-zhaoqing-intercity.vue
@@ -0,0 +1,58 @@
+<template>
+	<view>
+		<!-- <navigator url="/pages/line2/gzsrs2jhwg" delta="1">
+			<view class="line">
+				<view class="cn">
+					<text>广州南站——嘉禾望岗</text>
+				</view>
+				<view class="en">
+					<text>Guangzhou South Railway Station towards Jiahewanggang</text>
+				</view>
+			</view>
+		</navigator> -->
+		<navigator url="/pages/guangzhou-zhaoqing-intercity/xiaojinkou-zhaoqing" delta="1">
+			<view class="line">
+				<view class="cn">
+					<text>小金口——肇庆</text>
+				</view>
+				<view class="en">
+					<text>Xiaojinkou Railway Station towards Zhaoqing Railway Station</text>
+				</view>
+			</view>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	navigator {
+		width: initial;
+	}
+
+	.line {
+		height: 4.5834rem;
+		padding: 1% 2%;
+		width: auto;
+		color: #FFF;
+		background-color: rgb(42, 90, 166);
+		border-radius: 10rpx;
+
+		.col {
+			.cn {
+				font-size: 2.1rem;
+			}
+
+			.en {
+				font-size: 1.1rem;
+			}
+		}
+	}
+</style>
