9bdd98b4c0cdc47e51818f8cc442fedfc23b420e
15625104818 <103869422+Sgss123@users.noreply.github.com>
2025-02-15 17:25:23 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
 module.exports = {
 	transpileDependencies: ['@dcloudio/uni-ui'],
-	devServer: {
-		proxy: {
-			'/callback.html': {
-				target: 'http://localhost:8080', // 本地调试地址
-				changeOrigin: true,
-				secure: false
-			}
-		}
-	}
 }
