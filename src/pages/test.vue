049cb7122afe26899a77ac2e5859a303381e8654
MFJip <103869422+Sgss123@users.noreply.github.com>
2025-02-27 16:35:17 +0800

    Update test.vue

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,8 +1,8 @@
 <template>
 	<view class="container">
-		<!-- <button @click="loadFiles">加载广播文件</button>
-		 <!-- 页面中的按钮 -->
-		<button @click="handleLogin">登录并加载文件</button>-->
+		<!-- <button @click="loadFiles">加载广播文件</button> -->
+		<!-- 页面中的按钮 -->
+		<button @click="handleLogin">登录并加载文件</button>
 		<view v-if="loading" class="loading">加载中...</view>
 		<view v-else>
 			<view v-for="file in files" :key="file.name" class="file-item">
@@ -14,35 +14,35 @@
 </template>
 
 <script>
-	import oneDrive from '@/compoments/onedrive.js';
+import oneDrive from '@/compoments/onedrive.js';
 
-	export default {
-		data() {
-			return {
-				files: [],
-				loading: false
-			};
-		},
-		async mounted() {
+export default {
+	data() {
+		return {
+			files: [],
+			loading: false
+		};
+	},
+	async mounted() {
+		await oneDrive.init();
+	},
+	methods: {
+		async handleLogin() {
 			await oneDrive.init();
+			this.loadFiles();
 		},
-		methods: {
-			async handleLogin() {
-				await oneDrive.init();
-				this.loadFiles();
-			},
-			async loadFiles() {
-				this.loading = true;
-				try {
-					this.files = await oneDrive.getFileList();
-				} catch (error) {
-					uni.showToast({
-						title: '加载失败，请重试',
-						icon: 'none'
-					});
-				}
-				this.loading = false;
+		async loadFiles() {
+			this.loading = true;
+			try {
+				this.files = await oneDrive.getFileList();
+			} catch (error) {
+				uni.showToast({
+					title: '加载失败，请重试',
+					icon: 'none'
+				});
 			}
+			this.loading = false;
 		}
-	};
+	}
+};
 </script>
