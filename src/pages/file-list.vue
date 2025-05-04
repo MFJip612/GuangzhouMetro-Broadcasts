6f5b857c79650f9cddfc50e6d4d7091af0da629f
15625104818 <103869422+Sgss123@users.noreply.github.com>
2025-05-04 21:32:17 +0800

    feat: initialize project with basic HTML structure, TypeScript definitions for Vue, and Vite configuration

--- src/pages/file-list.vue
+++ src/pages/file-list.vue
@@ -4,129 +4,159 @@
 			<ul v-if="localContents.length">
 				<li v-for="item in localContents" :key="item.id">
 					<template v-if="item.folder">
-						<text>📁 {{ item.name }}</text>
-						<view class="sub-folder">
+						<text @click="toggleFolder(item)" class="folder-toggle">
+							{{ item.isExpanded ? '▼' : '▶' }} 📁 {{ item.name }}
+						</text>
+						<view v-show="item.isExpanded" class="sub-folder">
 							<FileTree :contents="item.children" />
 						</view>
 					</template>
 					<template v-else>
 						<text v-show="isViewName">
-							<uni-link class="file-link" :href="item['@microsoft.graph.downloadUrl']"
-								target="_blank">📄{{ item.name }}</uni-link>
+							<uni-link class="file-link" :href="item['@microsoft.graph.downloadUrl']" target="_blank">
+								📄 {{ item.name }}
+							</uni-link>
 						</text>
-						<audio v-show="!isViewName" :src="item['@microsoft.graph.downloadUrl']" :name="item.name"
-							controls></audio>
 					</template>
 				</li>
 			</ul>
-			<view v-else><text>加载中...</text></view>
+			<view v-else>
+				<text>加载中...</text>
+			</view>
 		</view>
 	</view>
 </template>
+
 <script>
+import { ref, reactive, watch, onMounted } from 'vue';
+
 export default {
-	name: 'FileTree',
-	props: {
-		contents: {
-			type: Array,
-			default: () => []
-		}
-	},
-	data() {
-		return {
-			accessToken: null,
-			localContents: [], // 使用 localContents 而不是直接修改 props
-			isViewName: true
-		};
-	},
-	async mounted() {
-		await this.init();
-	},
-	watch: {
-		// 监听 props.contents 的变化，更新 localContents
-		contents: {
-			immediate: true,
-			handler(newVal) {
-				this.localContents = newVal;
-			}
-		}
-	},
-	methods: {
-		change() {
-			this.isViewName = !this.isViewName;
-		},
-		async getAccessToken() {
-			try {
-				const response = await fetch('https://api.broadcast.mfjip.site/getAccessToken', {
-					method: 'POST',
-				});
-				const data = await response.json();
-				return data.access_token;
-			} catch (error) {
-				console.error('获取访问令牌失败:', error);
-				return null;
-			}
-		},
-		async getFolderContents(folderPath) {
-			const url =
-				`https://graph.microsoft.com/v1.0/users/0234abe4-65d4-4827-9625-bd8f7e26df03/drive/root:/${folderPath}:/children`;
-			let contents = [];
-			try {
-				let nextLink = url;
-				while (nextLink) {
-					const response = await fetch(nextLink, {
-						headers: {
-							'Authorization': `Bearer ${this.accessToken}`
-						},
-					});
-					const data = await response.json();
-					for (const item of data.value) {
-						if (item.folder) {
-							item.children = await this.getFolderContents(`${folderPath}/${item.name}`);
-						}
-						contents.push(item);
-					}
-					nextLink = data['@odata.nextLink'];
-				}
-			} catch (error) {
-				console.error('获取目录内容失败:', error);
-			}
-			return contents;
-		},
-		async init() {
-			// 如果 props.contents 为空，则初始化数据
-			if (this.contents.length === 0) {
-				this.accessToken = await this.getAccessToken();
-				if (this.accessToken) {
-					this.localContents = await this.getFolderContents('broadcast');
-				} else {
-					console.error('无法获取访问令牌');
-				}
-			} else {
-				// 如果 props.contents 不为空，则直接使用 props.contents
-				this.localContents = this.contents;
-			}
-		}
-	}
+    name: 'FileTree',
+    props: {
+        contents: {
+            type: Array,
+            default: () => []
+        }
+    },
+    setup(props) {
+        const accessToken = ref(null);
+        const localContents = ref([]);
+        const isViewName = ref(true);
+
+        // 初始化文件夹的展开状态
+        watch(
+            () => props.contents,
+            (newVal) => {
+                localContents.value = newVal.map((item) => ({
+                    ...item,
+                    isExpanded: false // 默认文件夹是收起状态
+                }));
+            },
+            { immediate: true }
+        );
+
+        const toggleFolder = (item) => {
+            item.isExpanded = !item.isExpanded;
+        };
+
+        const change = () => {
+            isViewName.value = !isViewName.value;
+        };
+
+        const getAccessToken = async () => {
+            try {
+                const response = await fetch('https://api.broadcast.mfjip.site/getAccessToken', {
+                    method: 'POST'
+                });
+                const data = await response.json();
+                return data.access_token;
+            } catch (error) {
+                console.error('获取访问令牌失败:', error);
+                return null;
+            }
+        };
+
+        const getFolderContents = async (folderPath) => {
+            const url = `https://graph.microsoft.com/v1.0/users/0234abe4-65d4-4827-9625-bd8f7e26df03/drive/root:/${folderPath}:/children`;
+            let contents = [];
+            try {
+                let nextLink = url;
+                while (nextLink) {
+                    const response = await fetch(nextLink, {
+                        headers: {
+                            Authorization: `Bearer ${accessToken.value}`
+                        }
+                    });
+                    const data = await response.json();
+                    for (const item of data.value) {
+                        if (item.folder) {
+                            item.children = await getFolderContents(`${folderPath}/${item.name}`);
+                            item.isExpanded = false; // 默认文件夹是收起状态
+                        }
+                        contents.push(item);
+                    }
+                    nextLink = data['@odata.nextLink'];
+                }
+            } catch (error) {
+                console.error('获取目录内容失败:', error);
+            }
+            return contents;
+        };
+
+        const init = async () => {
+            if (props.contents.length === 0) {
+                accessToken.value = await getAccessToken();
+                if (accessToken.value) {
+                    localContents.value = await getFolderContents('broadcast');
+                    localContents.value = localContents.value.map((item) => ({
+                        ...item,
+                        isExpanded: false // 默认文件夹是收起状态
+                    }));
+                } else {
+                    console.error('无法获取访问令牌');
+                }
+            } else {
+                localContents.value = props.contents.map((item) => ({
+                    ...item,
+                    isExpanded: false // 默认文件夹是收起状态
+                }));
+            }
+        };
+
+        onMounted(init);
+
+        return {
+            localContents,
+            isViewName,
+            toggleFolder,
+            change
+        };
+    }
 };
 </script>
+
 <style lang="scss" scoped>
 .file-link {
-	color: blue;
-	text-decoration: underline;
-	cursor: pointer;
+    color: blue;
+    text-decoration: underline;
+    cursor: pointer;
 }
 
 ul {
-	list-style-type: none;
-	// margin-left: 20px;
+    list-style-type: none;
+    padding-left: 20px;
 }
 
 li {
-	margin: 5px 0;
+    margin: 5px 0;
+}
+
+.folder-toggle {
+    cursor: pointer;
+    user-select: none;
 }
 
 .sub-folder {
-	// margin-left: 20px;
+    padding-left: 20px;
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,412 +1,411 @@
 <template>
-	<view class="content">
-		<uni-notice-bar show-icon scrollable single text="欢迎投稿更多线路! 本网站目前有1,2,3号线的线路报站,敬请期待其他线路! "></uni-notice-bar>
-		<image src="/static/images/Guangzhou_Metro_logo.svg" style="width: 100%;height: 80rpx;" mode="aspectFit">
-		</image>
-		<view class="choose">
-			<navigator url="/pages/line1/line1">
-				<view class="line line-1 color-black">
-					<text class="num">1</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 1</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator url="/pages/line2/line2">
-				<view class="line line-2">
-					<text class="num">2</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 2</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator url="/pages/line3/line3">
-				<view class="line line-3">
-					<text class="num">3</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 3</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator url="/pages/line4/line4">
-				<view class="line line-4 disable">
-					<text class="num">4</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 4</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator url="/pages/line5/line5">
-				<view class="line line-5">
-					<text class="num">5</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 5</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator url="/pages/line6/line6">
-				<view class="line line-6 disable">
-					<text class="num">6</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 6</text>
-					</view>
-				</view>
-			</navigator>
-			<view class="line line-7 disable">
-				<text class="num">7</text>
-				<view class="col">
-					<text class="cn">号线</text>
-					<text class="en">Line 7</text>
-				</view>
-			</view>
-			<navigator url="/pages/line8/line8">
-				<view class="line line-8 disable">
-					<text class="num">8</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 8</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-9 color-black disable">
-					<text class="num">9</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 9</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator url="">
-				<view class="line line-11 color-black disable">
-					<text class="num">11</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 11</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-13 disable">
-					<text class="num">13</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 13</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-14 disable">
-					<text class="num">14</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 14</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-18 disable">
-					<text class="num">18</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 18</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-21 disable">
-					<text class="num">21</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 21</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-22 disable">
-					<text class="num">22</text>
-					<view class="col">
-						<text class="cn">号线</text>
-						<text class="en">Line 22</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-gf disable">
-					<view class="col">
-						<text class="cn">广佛线</text>
-						<text class="en">Guangfo Line</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-apm disable">
-					<text class="num">APM</text>
-					<view class="col">
-						<text class="cn">线</text>
-						<text class="en">Line</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-intercity guangqing disable">
-					<view class="col">
-						<text class="cn">广清城际</text>
-						<text class="en">Guangzhou-Qingyuan Intercity</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-intercity donghuan disable">
-					<view class="col">
-						<text class="cn">广州东环城际</text>
-						<text class="en">Guangzhou East Ring Intercity</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator>
-				<view class="line line-intercity disable">
-					<view class="col">
-						<text class="cn">广惠城际</text>
-						<text class="en">Guangzhou-Huizhou Intercity</text>
-					</view>
-				</view>
-			</navigator>
-			<navigator url="/pages/guangzhou-zhaoqing-intercity/guangzhou-zhaoqing-intercity">
-				<view class="line line-intercity">
-					<view class="col">
-						<text class="cn">广肇城际</text>
-						<text class="en">Guangzhou-Zhaoqing Intercity</text>
-					</view>
-				</view>
-			</navigator>
-		</view>
-		<view class="text-area"
-			style="justify-content: center;display: flex;flex-direction: column;align-items: center;">
-			<text class="title">由 <uni-link href="https://blog.zhengxian.top/" text="@380AM-0204">@380AM-0204</uni-link>
-				提供文件服务</text>
-			<text class="title">由 <uni-link href="https://space.bilibili.com/9033373/" text="@红茶w">@红茶w</uni-link>
-				提供报站资源</text>
-			<text class="title">
-				<navigator url="/pages/file-list">广播文件</navigator>
-			</text>
-			<text class="title"><uni-link href="https://forms.office.com/r/15t2jNb82h">
-					<image src="/static/images/QRCode.png" mode="aspectFit"></image>
-				</uni-link></text>
-		</view>
-	</view>
+  <view class="content">
+    <uni-notice-bar show-icon scrollable single text="欢迎投稿更多线路! 本网站目前有1,2,3号线的线路报站,敬请期待其他线路! "></uni-notice-bar>
+    <image src="/static/images/Guangzhou_Metro_logo.svg" style="width: 100%;height: 80rpx;" mode="aspectFit">
+    </image>
+    <view class="choose">
+      <navigator url="/pages/line1/line1">
+        <view class="line line-1 color-black">
+          <text class="num">1</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 1</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator url="/pages/line2/line2">
+        <view class="line line-2">
+          <text class="num">2</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 2</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator url="/pages/line3/line3">
+        <view class="line line-3">
+          <text class="num">3</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 3</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator url="/pages/line4/line4">
+        <view class="line line-4 disable">
+          <text class="num">4</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 4</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator url="/pages/line5/line5">
+        <view class="line line-5">
+          <text class="num">5</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 5</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator url="/pages/line6/line6">
+        <view class="line line-6 disable">
+          <text class="num">6</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 6</text>
+          </view>
+        </view>
+      </navigator>
+      <view class="line line-7 disable">
+        <text class="num">7</text>
+        <view class="col">
+          <text class="cn">号线</text>
+          <text class="en">Line 7</text>
+        </view>
+      </view>
+      <navigator url="/pages/line8/line8">
+        <view class="line line-8 disable">
+          <text class="num">8</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 8</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-9 color-black disable">
+          <text class="num">9</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 9</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator url="">
+        <view class="line line-11 color-black disable">
+          <text class="num">11</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 11</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-13 disable">
+          <text class="num">13</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 13</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-14 disable">
+          <text class="num">14</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 14</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-18 disable">
+          <text class="num">18</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 18</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-21 disable">
+          <text class="num">21</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 21</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-22 disable">
+          <text class="num">22</text>
+          <view class="col">
+            <text class="cn">号线</text>
+            <text class="en">Line 22</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-gf disable">
+          <view class="col">
+            <text class="cn">广佛线</text>
+            <text class="en">Guangfo Line</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-apm disable">
+          <text class="num">APM</text>
+          <view class="col">
+            <text class="cn">线</text>
+            <text class="en">Line</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-intercity guangqing disable">
+          <view class="col">
+            <text class="cn">广清城际</text>
+            <text class="en">Guangzhou-Qingyuan Intercity</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-intercity donghuan disable">
+          <view class="col">
+            <text class="cn">广州东环城际</text>
+            <text class="en">Guangzhou East Ring Intercity</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator>
+        <view class="line line-intercity disable">
+          <view class="col">
+            <text class="cn">广惠城际</text>
+            <text class="en">Guangzhou-Huizhou Intercity</text>
+          </view>
+        </view>
+      </navigator>
+      <navigator url="/pages/guangzhou-zhaoqing-intercity/guangzhou-zhaoqing-intercity">
+        <view class="line line-intercity">
+          <view class="col">
+            <text class="cn">广肇城际</text>
+            <text class="en">Guangzhou-Zhaoqing Intercity</text>
+          </view>
+        </view>
+      </navigator>
+    </view>
+    <view class="text-area" style="justify-content: center;display: flex;flex-direction: column;align-items: center;">
+      <text class="title">由 <uni-link href="https://blog.zhengxian.top/" text="@380AM-0204">@380AM-0204</uni-link>
+        提供文件服务</text>
+      <text class="title">由 <uni-link href="https://space.bilibili.com/9033373/" text="@红茶w">@红茶w</uni-link>
+        提供报站资源</text>
+      <text class="title">
+        <navigator url="/pages/file-list">广播文件</navigator>
+      </text>
+      <text class="title"><uni-link href="https://forms.office.com/r/15t2jNb82h">
+          <image src="/static/images/QRCode.png" mode="aspectFit"></image>
+        </uni-link></text>
+    </view>
+  </view>
 </template>
 
 <script>
-	// import '/static/js/protect.js'
-	export default {
-		data() {
-			return {
+// import '/static/js/protect.js'
+export default {
+  data() {
+    return {
 
-			}
-		},
-		onLoad() {},
-		methods: {
+    }
+  },
+  onLoad() { },
+  methods: {
 
-		}
-	}
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-	navigator {
-		width: initial;
-	}
+navigator {
+  width: initial;
+}
 
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
 
-	.choose {
-		width: 25rem;
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		flex-wrap: wrap;
-		justify-content: space-around;
-		// margin: 50rpx;
-	}
+.choose {
+  width: 25rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  // margin: 50rpx;
+}
 
-	.line {
-		margin: 10rpx;
-		position: relative;
-		width: 280rpx;
-		border-radius: 20rpx;
-		display: flex;
-		text-align: center;
-		justify-content: center;
-		align-items: center;
-		color: #FFF;
+.line {
+  margin: 10rpx;
+  position: relative;
+  width: 280rpx;
+  border-radius: 20rpx;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  color: #FFF;
 
-		.num {
-			position: relative;
-			text-align: center;
-			font-size: 4rem;
-			font-family: 'Arial';
-		}
+  .num {
+    position: relative;
+    text-align: center;
+    font-size: 4rem;
+    font-family: 'Arial';
+  }
 
-		.col {
-			position: relative;
-			display: flex;
-			flex-direction: column;
-			text-align: center;
+  .col {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
 
-			.cn {
-				font-size: 2rem;
-				font-family: '黑体';
-			}
+    .cn {
+      font-size: 2rem;
+      font-family: '黑体';
+    }
 
-			.en {
-				font-size: 1.2rem;
-				font-family: 'Arial';
-			}
-		}
-	}
+    .en {
+      font-size: 1.2rem;
+      font-family: 'Arial';
+    }
+  }
+}
 
-	.color-white {
-		color: #FFF;
-	}
+.color-white {
+  color: #FFF;
+}
 
-	.color-black {
-		color: #000;
-	}
+.color-black {
+  color: #000;
+}
 
-	.line-1 {
-		background-color: #F3D03E;
-	}
+.line-1 {
+  background-color: #F3D03E;
+}
 
-	.line-2 {
-		color: #FFF;
-		background-color: #00629B;
-	}
+.line-2 {
+  color: #FFF;
+  background-color: #00629B;
+}
 
-	.line-3 {
-		color: #FFF;
-		background-color: #ECA154;
-	}
+.line-3 {
+  color: #FFF;
+  background-color: #ECA154;
+}
 
-	.line-4 {
-		color: #FFF;
-		background-color: #00843D;
-	}
+.line-4 {
+  color: #FFF;
+  background-color: #00843D;
+}
 
-	.line-5 {
-		color: #FFF;
-		background-color: #C5003E;
-	}
+.line-5 {
+  color: #FFF;
+  background-color: #C5003E;
+}
 
-	.line-6 {
-		color: #FFF;
-		background-color: #80225F;
-	}
+.line-6 {
+  color: #FFF;
+  background-color: #80225F;
+}
 
-	.line-7 {
-		color: #FFF;
-		background-color: #97D700;
-	}
+.line-7 {
+  color: #FFF;
+  background-color: #97D700;
+}
 
-	.line-8 {
-		color: #FFF;
-		background-color: #008C95;
-	}
+.line-8 {
+  color: #FFF;
+  background-color: #008C95;
+}
 
-	.line-9 {
-		// color: #FFF;
-		background-color: #71CC98;
-	}
+.line-9 {
+  // color: #FFF;
+  background-color: #71CC98;
+}
 
-	.line-11 {
-		background-color: #ffb00a;
-	}
+.line-11 {
+  background-color: #ffb00a;
+}
 
-	.line-13 {
-		color: #FFF;
-		background-color: #8E8C13;
-	}
+.line-13 {
+  color: #FFF;
+  background-color: #8E8C13;
+}
 
-	.line-14 {
-		color: #FFF;
-		background-color: #81312F;
-	}
+.line-14 {
+  color: #FFF;
+  background-color: #81312F;
+}
 
-	.line-18 {
-		color: #FFF;
-		background-color: #0047BA;
-	}
+.line-18 {
+  color: #FFF;
+  background-color: #0047BA;
+}
 
-	.line-21 {
-		color: #FFF;
-		background-color: #211747;
-	}
+.line-21 {
+  color: #FFF;
+  background-color: #211747;
+}
 
-	.line-22 {
-		color: #FFF;
-		background-color: #CD5228;
-	}
+.line-22 {
+  color: #FFF;
+  background-color: #CD5228;
+}
 
-	.line-gf {
-		height: 4.5834rem;
-		color: #FFF;
-		background-color: #C4D600;
+.line-gf {
+  height: 4.5834rem;
+  color: #FFF;
+  background-color: #C4D600;
 
-		.col {
-			.cn {
-				font-size: 2.3rem;
-			}
+  .col {
+    .cn {
+      font-size: 2.3rem;
+    }
 
-			.en {
-				font-size: 1.25rem;
-			}
-		}
-	}
+    .en {
+      font-size: 1.25rem;
+    }
+  }
+}
 
-	.line-apm {
-		height: 4.5834rem;
-		color: #FFF;
-		background-color: #00B5E2;
+.line-apm {
+  height: 4.5834rem;
+  color: #FFF;
+  background-color: #00B5E2;
 
-		.num {
-			font-size: 2.85rem;
-		}
-	}
+  .num {
+    font-size: 2.85rem;
+  }
+}
 
-	.line-intercity {
-		width: 20rem;
-		height: 4.5834rem;
-		color: #FFF;
-		background-color: rgb(42, 90, 166);
-		border-radius: 10rpx;
+.line-intercity {
+  width: 20rem;
+  height: 4.5834rem;
+  color: #FFF;
+  background-color: rgb(42, 90, 166);
+  border-radius: 10rpx;
 
-		.col {
-			.cn {
-				font-size: 2.1rem;
-			}
+  .col {
+    .cn {
+      font-size: 2.1rem;
+    }
 
-			.en {
-				font-size: 1.1rem;
-			}
-		}
-	}
+    .en {
+      font-size: 1.1rem;
+    }
+  }
+}
 
-	.disable {
-		color: #FFF;
-		background-color: gray;
-	}
+.disable {
+  color: #FFF;
+  background-color: gray;
+}
 </style>
